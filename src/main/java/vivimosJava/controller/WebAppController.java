04a4493f44a67c9fb327e603cc49dec976cbf778
Peivand <peivandp@gmail.com>
2021-05-01 18:55:56 -0400

    Se crea webapp controller

---
+++ src/main/java/vivimosJava/controller/WebAppController.java
@@ -0,0 +1,12 @@
+package vivimosJava.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebAppController {
+	@RequestMapping("/index")
+	public String getIndex() {
+		return "index";
+	}
+}
