8dffa9992bf1e0cf3cc4a52520b71e9a2c89d95a
Peivand <peivandp@gmail.com>
2021-05-19 13:38:47 -0400

    se añaden campos a clase recaptcha response

--- src/main/java/vivimosJava/controller/ReCaptchaResponse.java
+++ src/main/java/vivimosJava/controller/ReCaptchaResponse.java
@@ -1,13 +1,22 @@
 package vivimosJava.controller;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class ReCaptchaResponse {
 	private boolean success;
+	private float score;
+	private String action;
 	private String challenge_ts;
 	private String hostName;
+	
+	
+	@JsonProperty("error-codes")
+	private String[] errorCodes;
 	
 	public boolean isSuccess() {
 		return success;
 	}
+	
 	public void setSuccess(boolean success) {
 		this.success = success;
 	}
@@ -22,6 +31,18 @@ public class ReCaptchaResponse {
 	}
 	public void setHostName(String hostName) {
 		this.hostName = hostName;
+	}
+	public float getScore() {
+		return score;
+	}
+	public void setScore(float score) {
+		this.score = score;
+	}
+	public String getAction() {
+		return action;
+	}
+	public void setAction(String action) {
+		this.action = action;
 	}
 	
 	
