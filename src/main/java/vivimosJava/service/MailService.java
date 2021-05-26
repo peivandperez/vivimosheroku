3312754ca59d863e51cb03e6414c33da808fcc99
Peivand <peivandp@gmail.com>
2021-05-26 13:00:22 -0400

    Se hace async para que muestre la página y luego mande el mail

--- src/main/java/vivimosJava/service/MailService.java
+++ src/main/java/vivimosJava/service/MailService.java
@@ -3,6 +3,7 @@ package vivimosJava.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,7 +16,7 @@ public class MailService {
 		}
 	
 	
-	
+	@Async
 	public void sendEmail(String toMail,String subject,String text) {
 	
 		
